4539d165b54172ec3f376fa7ddc130bb4ccd3123
Ratmir Gaitov <gaitovrat@gmail.com>
2024-04-07 18:39:30 +0200

    Update

--- install.sh
+++ install.sh
@@ -20,6 +20,7 @@ if [ ! $(which brew) ]; then
     echo Installing brew...
     /bin/bash -c \
         "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> $HOME/.zprofile
 fi
 
 if [ ! -e "$HOME/.sdkman" ]; then
